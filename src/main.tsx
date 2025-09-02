0939278552ec099b6f09999ae5252efc45a4b874
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-09-02 16:18:00 +0300

    feat: init and pwa set

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
+import App from './App.tsx'
+import './index.css'
+
+// Register service worker
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    if (confirm('Доступно обновление. Перезагрузить?')) {
+      updateSW()
+    }
+  },
+  onOfflineReady() {
+    console.log('Приложение готово к работе офлайн')
+  },
+  onRegisteredSW(swUrl) {
+    console.log('Service Worker registered:', swUrl)
+  }
+})
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
